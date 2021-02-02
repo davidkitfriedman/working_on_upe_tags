3a3563a31e0fea9e40d3cc9fe03f164cce146a21
David Kit Friedman <david.kit.friedman@gmail.com>
2021-02-02 18:50:24 -0500

    Add files via upload

---
+++ total_edit_counts_for_socks.py
@@ -0,0 +1,33 @@
+import webbrowser
+import sys
+
+not_confirmed = ['GuardLine9', 'PledgeGrant']
+suspected_sock_puppets = ['VentureKit',\
+                          'Quorum816',\
+                          'JP Miller1',\
+                          'QuibbleCod',\
+                          'Accountmetric',\
+                          'Boothit11',\
+                          'Marginofinterest',\
+                          'GroundFloor',\
+                          'GuardLine9',\
+                          'PledgeGrant',\
+                          'Greente28',\
+                          'L0calh0$t',\
+                          'Balle010',\
+                          'WonderfulWorld',\
+                          'Deadbolt44']
+confirmed_sock_puppets = [ i for i in suspected_sock_puppets if not (i in not_confirmed)]
+
+for i in range(len(confirmed_sock_puppets)):
+    s = 'https://xtools.wmflabs.org/ec/en.wikipedia.org/' + \
+        confirmed_sock_puppets[i]        
+    print('{:02d}'.format(i+1) + ". " + confirmed_sock_puppets[i])
+    print('{:02d}'.format(i+1) + ".  Edit count information: " + confirmed_sock_puppets[i])
+    webbrowser.open_new_tab(s)
+    print("Press any key to continue...")
+    if ( sys.version[0] == '3'):
+        input()
+    elif ( sys.version[0] == '2'):
+        raw_input()
+
